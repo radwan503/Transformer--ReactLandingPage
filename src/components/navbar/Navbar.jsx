d2667969ee562fb45ca7507b9d3d82b14299c732
Radwan Ahmed Anik <radwancse503@gmail.com>
2021-12-04 21:49:30 +0600

    header section done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { RiMenu3Fill, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 import './navbar.css'
 
+
+const Menu = () => (
+   <>
+      <p><a href="#home"> Home</a></p>
+      <p><a href="#transformer"> Transformer</a></p>
+      <p><a href="#possibolity"> Open AI</a></p>
+      <p><a href="#feature"> Case Study</a></p>
+      <p><a href="#blog"> Library</a></p>
+   </>
+)
+
 const Navbar = () => {
+   const [toogleMenu, setToggleMenu] = useState(false);
+
    return (
-      <div>
-         navbartt
+      <div className="transformer__navbar">
+         <div className="transformer__navbar-links">
+            <div className="transformer__navbar-links_logo">
+               <img src={logo} alt="tranformer logo"></img>
+            </div>
+            <div className="transformer__navbar-links_container">
+               <Menu />
+            </div>
+         </div>
+         <div className="transformer__navbar-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+         </div>
+
+         <div className="transformer__navbar-menu">
+
+            {toogleMenu ?
+               <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+               : <RiMenu3Fill color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+            }
+            {
+               toogleMenu && (
+                  <div className="transformer__navbar-menu_container scale-up-center">
+                     <div className="transformer__navbar-menu_container-links">
+                        <Menu />
+
+                        <div className="transformer__navbar-menu_container-links-sign">
+                           <p>Sign in</p>
+                           <button type="button">Sign up</button>
+                        </div>
+                     </div>
+
+                  </div>
+               )
+            }
+
+         </div>
       </div>
    )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+
 import './header.css'
 
 const Header = () => {
    return (
-      <div>
-         header
+      <div className="transformer__header section__padding" id="home">
+         <div className="transformer__header-content">
+            <h1 className="gradient__text">Let’s Build Something amazing with TransformerAI</h1>
+            <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+            <div className="transformer__header-content__input">
+               <input type="email" placeholder="Enter Your Email"/>
+               <button type="button">Get Started</button>
+            </div>
+
+            <div className="transformer__header-content__people">
+            <img src={people} alt="transformer people"/>
+            <p>1,600 people requested access a visit in last 24 hours</p>
+            </div>
+         </div>
+         <div className="transformer__header-image">
+               <figure>
+                  <img src={ai} alt="transformer ai"/>
+               </figure>
+         </div>
       </div>
    )
 }
